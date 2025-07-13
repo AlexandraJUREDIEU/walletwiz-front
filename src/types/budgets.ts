7cd619dcc6e22bace2088a59655c64812f1cf621
Alexandra Juredieu <theuleaua@gmail.com>
2025-07-13 23:29:18 +0200

    fix(types): add incomes and expenses to Budget type definition

--- src/types/budgets.ts
+++ src/types/budgets.ts
@@ -1,9 +1,13 @@
+import type { Expense } from "./expenses"
+import type { Income } from "./incomes"
 import type { Transaction } from "./transactions"
 
 export type Budget = {
   id: string
   month: string // format '2025-07'
   createdAt: string
+  incomes?: Income[]
+  expenses?: Expense[]
   allocations: Allocation[]
   transactions: Transaction[]
 }
